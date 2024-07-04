eb773604ba527167d42a8fdf4bae97adaa07496e
X-Treme123 <faizalputraramadhan29@gmail.com>
2024-07-04 08:20:02 +0700

    Perubahan Commit

--- src/Components/Fragments/NavbarLogin.jsx
+++ src/Components/Fragments/NavbarLogin.jsx
@@ -14,13 +14,15 @@ const NavbarLogin =  () => {
 
     return(
         <div className="h-screen">
-            <navbar className="bg-orange-500 text-white flex justify-between px-10 h-20 items-center">
+            <navbar className="bg-green-500 text-white flex justify-between px-10 h-20 items-center">
                 <h1 className="text-2xl font-bold">TrendMart</h1>
                 <Button onClick={handleLogin}>Login</Button>
             </navbar>
-            <div className="flex justify-center items-center flex-col flex-grow h-3/4 background-image drop-shadow-md">
-                <h1 className="font-bold text-3xl text-white sh">Welcome To TrendMart</h1>
-                <h1 className="font-bold text-3xl text-green-400 mt-2 mb-5">Enjoy Shopping Enjoy Your Life</h1>
+            <div className="border border-slate-400 flex justify-center items-center flex-col flex-grow background-image drop-shadow-md">
+                <div className="flex justify-center items-center flex-col px-8 py-8 radius rounded-lg backdrop-blur-sm">
+                    <h1 className="font-bold text-3xl text-black sh">Welcome To TrendMart</h1>
+                    <h1 className="font-bold text-3xl text-green-500 mt-2 mb-5">Enjoy Shopping Enjoy Your Life</h1>
+                </div>
             </div>
         </div>
     )
